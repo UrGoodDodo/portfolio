61accdc343d8971a07a10f8784dfef3af779b2d7
Kirill <kir_zinn@mail.ru>
2026-08-30 17:34:54 +0300

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://urgooddodo.github.io',
+  base: '/portfolio',
+
   i18n: {
     locales: ['en', 'ru'],
 
